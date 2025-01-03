80ed30969b6fd849196126794da7b1049dbcd672
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-01-03 12:37:27 +0100

    Chiamata Axios per recuperare API e map per elenco pokémon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [characters, setCharacters] = useState([])
+
+  function fetchCharacters() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then(res => {
+        console.log(res)
+        setCharacters(res.data.results)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  useEffect(() => {
+    fetchCharacters()
+  }, [])
 
   return (
     <>
-      <h1>Pokémon</h1>
+      <div className='container'>
+        <h1>Pokémon</h1>
+      </div>
+      <div>
+        <ul>
+          {characters.map((character) => (
+            <li key={character.id}>{character.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
